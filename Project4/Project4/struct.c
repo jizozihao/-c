ee738266ef221fe282a89076dd2b8fac455d1b15
焦子豪 <96931383+jzh-ry@users.noreply.github.com>
2022-05-11 21:22:08 +0800

    Add files via upload

--- Project4/Project4/struct.c
+++ Project4/Project4/struct.c
@@ -244,3 +244,179 @@
 //}
 
 
+//柔性数组
+
+//struct S
+//{
+//	int n;
+//	//数组的大小可变
+//	int arr[0];//柔性数组在结构体的最后数组可以是未知大小
+//};
+//
+//
+//int main()
+//{
+//	struct S s;
+//	printf("%d\n", sizeof(s));
+//	struct S* ps = malloc(sizeof(struct S)+5*sizeof(int));
+//	ps->n = 100;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	struct S* ptr = realloc(ps, 44);
+//	if (ptr != NULL)
+//	{
+//		ps = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+
+
+//struct S
+//{
+//	int n;
+//	int* arr;
+//};
+//int main()
+//{
+//	struct S* ps = (struct S*)malloc(sizeof(struct S));
+//	ps->arr = malloc(5 * sizeof(int));
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{	
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//
+//	//调整大小
+//	int* ptr = realloc(ps->arr, 10 * sizeof(int));
+//	if (ptr != NULL)
+//	{
+//		ps->arr = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//	free(ps->arr);
+//	ps = NULL;
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+
+
+//struct S
+//{
+//	int n;
+//	char c;
+//	int arr[0];//表示一个柔性数组，只能放在结构体末尾
+//};
+//
+//int main()
+//{
+//	struct S* p = (struct S*)malloc(sizeof(struct S) + 10 * sizeof(int));
+//	p->n = 100;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		p->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", p->arr[i]);
+//	}
+//	printf("\n");
+//	//调整arr空间
+//	struct S* ptr = realloc(p, sizeof(struct S) + 20 * sizeof(int));
+//	if (ptr != NULL)
+//	{
+//		p = ptr;
+//	}
+//	for (i = 10; i < 20; i++)
+//	{
+//		p->arr[i] = i;
+//	}
+//	for (i = 10; i < 20; i++)
+//	{
+//		printf("%d ", p->arr[i]);
+//	}
+//	free(p);
+//	p = NULL;
+//	
+//	return 0;
+//}
+
+
+
+
+
+struct S
+{
+	int n;
+	int* arr;
+};
+
+
+int main()
+{
+	struct S* p = (struct S*)malloc(sizeof(struct S));
+	p->n = 100;
+	p->arr = (int*)malloc(10 * sizeof(int));
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		p->arr[i] = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", p->arr[i]);
+	}
+	printf("\n");
+	//扩展
+	int* ptr = realloc(p->arr, 20 * sizeof(int));
+	if (ptr != NULL)
+	{
+		p->arr = ptr;
+	}
+	
+	for (i = 10; i < 20; i++)
+	{
+		p->arr[i] = i;
+	}
+	for (i = 0; i < 20; i++)
+	{
+		printf("%d ", p->arr[i]);
+	}
+	free(p->arr);
+	p->arr = NULL;
+
+
+
+
+
+
+
+	return 0;
+}
+
+
